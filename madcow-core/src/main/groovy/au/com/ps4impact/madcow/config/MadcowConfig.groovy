91a0d3dd714916e4ecb9b839e09fb9acf7f6a127
Gavin Bunney <gavin.bunney@gmail.com>
2012-02-08 13:48:37 +1000

    Added skeleton config

--- madcow-core/src/main/groovy/au/com/ps4impact/madcow/config/MadcowConfig.groovy
+++ madcow-core/src/main/groovy/au/com/ps4impact/madcow/config/MadcowConfig.groovy
@@ -6,5 +6,12 @@ package au.com.ps4impact.madcow.config
  */
 class MadcowConfig {
 
+    static Node ConfigData;
     static String StepRunner = "au.com.ps4impact.madcow.runner.webdriver.WebDriverStepRunner";
+
+    public static void parseConfig(String configXML) {
+        ConfigData = new XmlParser().parseText(configXML);
+        StepRunner = ConfigData.execution.runner.text();
+        // TODO - validate config data
+    }
 }
