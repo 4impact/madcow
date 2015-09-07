dc22dfab42a83847c84a90021053363c78cc2a5e
paulbevis <paulbevis2010@gmail.com>
2015-09-07 11:38:53 +1000

    SelectField now stale checks is sub elements (option) before accessing

--- madcow-webdriver-plugin/src/main/groovy/au/com/ps4impact/madcow/runner/webdriver/blade/SelectField.groovy
+++ madcow-webdriver-plugin/src/main/groovy/au/com/ps4impact/madcow/runner/webdriver/blade/SelectField.groovy
@@ -27,8 +27,12 @@ import au.com.ps4impact.madcow.runner.webdriver.WebDriverStepRunner
 import au.com.ps4impact.madcow.step.MadcowStep
 import au.com.ps4impact.madcow.step.MadcowStepResult
 import org.openqa.selenium.By
+import org.openqa.selenium.StaleElementReferenceException
+import org.openqa.selenium.WebDriver
 import org.openqa.selenium.WebElement
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedCondition
+import org.openqa.selenium.support.ui.Select
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * The SelectField blade sets a value on a combo box element.
@@ -42,24 +46,24 @@ class SelectField extends WebDriverBladeRunner {
 
         if (Integer.class.isInstance(step.blade.parameters)) {
             int item = step.blade.parameters as int;
-            step.result = makeASingleSelection(element, item)
+            step.result = makeASingleSelection(stepRunner, step, element, item)
         } else if (List.class.isInstance(step.blade.parameters)) {
             //check if it is multi select
             def selectField = new Select(element)
-            if (selectField.isMultiple()){
+            if (selectField.isMultiple()) {
                 List<String> paramMap = step.blade.parameters as List;
                 def tempResult = [:]
                 paramMap.each { selection ->
                     //try to find it and select it
-                    tempResult.put(selection, makeASingleSelection(element, selection.trim()));
+                    tempResult.put(selection, makeASingleSelection(stepRunner, step, element, selection.trim()));
                 }
-                def failedItems = tempResult.findAll{ entry ->
-                    if (!(entry.value as MadcowStepResult).passed()){
+                def failedItems = tempResult.findAll { entry ->
+                    if (!(entry.value as MadcowStepResult).passed()) {
                         return entry.key
                     }
                 }.keySet()
                 if (!failedItems.isEmpty()) {
-                    step.result = MadcowStepResult.FAIL('Cannot find a valid option/s for item '+failedItems.toString());
+                    step.result = MadcowStepResult.FAIL('Cannot find a valid option/s for item ' + failedItems.toString());
                     return
                 }
                 step.result = MadcowStepResult.PASS()
@@ -68,57 +72,91 @@ class SelectField extends WebDriverBladeRunner {
                 step.result = MadcowStepResult.FAIL('Cannot specify list when select element doesn\'t have multiple attribute');
                 return
             }
-        }else {
+        } else {
             String text = step.blade.parameters as String;
-            step.result = makeASingleSelection(element, text.trim())
+            step.result = makeASingleSelection(stepRunner, step, element, text.trim())
         }
 
     }
 
-    private MadcowStepResult makeASingleSelection(WebElement element, int selectParam) {
-        List<WebElement> options = element.findElements(By.tagName('option'));
-        boolean foundIndex = false;
+    private MadcowStepResult makeASingleSelection(WebDriverStepRunner stepRunner, MadcowStep step, WebElement element, int selectParam) {
+        boolean result = new WebDriverWait(stepRunner.driver, 10, 1000)
+                .ignoring(StaleElementReferenceException.class)
+                .until(new ExpectedCondition() {
+            Boolean apply(WebDriver driver) {
+                try {
+                    List<WebElement> options = element.findElements(By.tagName('option'));
+                    boolean foundIndex = false;
 
-        if (selectParam <= options.size()){
-            foundIndex = true;
-        }
+                    if (selectParam <= options.size()) {
+                        foundIndex = true;
+                    }
 
-        if (foundIndex) {
-            new Select(element).selectByIndex(selectParam)
+                    if (foundIndex) {
+                        new Select(element).selectByIndex(selectParam)
+                        return Boolean.valueOf(true)
+                    }
+                }
+                catch (StaleElementReferenceException ex) {
+                    println('Stale reference caught, retrying...')
+                    element = findElement(stepRunner, step)
+                    return Boolean.valueOf(false);
+                }
+            }
+        })
+        if (result) {
             return MadcowStepResult.PASS();
         } else {
-            return MadcowStepResult.FAIL('Unable to find the specified option');
+            return MadcowStepResult.FAIL('Unable to update specified option');
+
         }
     }
 
-    private MadcowStepResult makeASingleSelection(WebElement element, String selectParam) {
-        List<WebElement> options = element.findElements(By.tagName('option'));
-        WebElement foundText = null;
-        WebElement foundValue = null;
+    private MadcowStepResult makeASingleSelection(WebDriverStepRunner stepRunner, MadcowStep step, WebElement element, String selectParam) {
+        boolean result = new WebDriverWait(stepRunner.driver, 10, 1000)
+                .ignoring(StaleElementReferenceException.class)
+                .until(new ExpectedCondition() {
+            Boolean apply(WebDriver driver) {
+                try {
+                    List<WebElement> options = element.findElements(By.tagName('option'));
+                    WebElement foundText = null;
+                    WebElement foundValue = null;
 
-        options.each { option ->
-            //if already found option then skip checking rest of options
-            if (foundText || foundValue)
-                return;
+                    options.each { option ->
+                        //if already found option then skip checking rest of options
+                        if (foundText || foundValue)
+                            return;
 
-            //first attempt to match on text value
-            if (option.text.trim() == selectParam) {
-                foundText = option;
+                        //first attempt to match on text value
+                        if (option.text.trim() == selectParam) {
+                            foundText = option;
+                        }
+                        //then attempt to match on value value
+                        if (option.getAttribute("value").trim() == selectParam) {
+                            foundValue = option;
+                        }
+                    }
+
+                    if (foundText != null) {
+                        new Select(element).selectByVisibleText(selectParam)
+                        return Boolean.valueOf(true)
+                    } else if (foundValue != null) {
+                        new Select(element).selectByValue(selectParam)
+                        return Boolean.valueOf(true)
+                    } else {
+                        return Boolean.valueOf(false)
+                    }
+                } catch (StaleElementReferenceException ex) {
+                    println('Stale reference caught, retrying...')
+                    element = findElement(stepRunner, step)
+                    return Boolean.valueOf(false)
+                }
             }
-            //then attempt to match on value value
-            if (option.getAttribute("value").trim() == selectParam) {
-                foundValue = option;
-            }
-        }
-
-        if (foundText != null) {
-            new Select(element).selectByVisibleText(selectParam)
-            return MadcowStepResult.PASS();
-        } else if (foundValue != null) {
-            new Select(element).selectByValue(selectParam)
+        });
+        if (result) {
             return MadcowStepResult.PASS();
         } else {
-            return MadcowStepResult.FAIL('Unable to find the specified option');
+            return MadcowStepResult.FAIL('Unable to update specified option');
         }
     }
 
@@ -130,7 +168,7 @@ class SelectField extends WebDriverBladeRunner {
         return [WebDriverBladeRunner.BLADE_MAPPING_SELECTOR_TYPE.HTMLID,
                 WebDriverBladeRunner.BLADE_MAPPING_SELECTOR_TYPE.NAME,
                 WebDriverBladeRunner.BLADE_MAPPING_SELECTOR_TYPE.XPATH,
-                WebDriverBladeRunner.BLADE_MAPPING_SELECTOR_TYPE.CSS ];
+                WebDriverBladeRunner.BLADE_MAPPING_SELECTOR_TYPE.CSS];
     }
 
     protected List<Class> getSupportedParameterTypes() {
